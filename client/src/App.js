7f908f4e210f3f763f7c5230d4eb844ac1efc26f
clark <clark_mitchelljr@hotmail.com>
2023-01-10 11:00:59 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+ const [course, setCourse] = useState([]);
+useEffect(() => {
+  fetch("http://localhost:5000/api/courses")
+  .then((res) => res.json())
+  .then((data) => setCourse(data));
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {course.map((course) => {
+    return (
+    <ul>
+      <li>{course.title}</li>
+    </ul>
+    )
+    })}
     </div>
   );
 }
